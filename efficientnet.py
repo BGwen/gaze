ee91c3297df48eeabb1931012e86b3b85ff00999
BGwen <92027791+BGwen@users.noreply.github.com>
2024-04-24 17:13:33 +0200

    module import

--- efficientnet.py
+++ efficientnet.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torchvision
 
-from efficientnet_pytorch import EfficientNet
+from gaze.efficientnet_pytorch.model import EfficientNet
 
 
 from .normalizer import Normalizer
